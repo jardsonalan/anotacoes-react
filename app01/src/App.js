1404e239a53c0895b43d834d68997d09bfcd531c
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-09-30 15:40:05 -0300

    update: criação de componentes

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import LogoServ from './components/imgs/react.png';
+import Header from './components/Header.js'; // Importação de componente
+import Corpo from './components/Corpo.js';
+// import LogoServ from './components/imgs/react.png';
 
 export default function App() {
-  const canal = () => {
-    return 'CFB Cursos';
-  };
-  function curso() {
-    return 'Curso de React';
-  };
-  // const canal = 'CFB Cursos';
-  // const curso = 'Curso de React';
   return (
-    <section>
-      <header>
-        <p>{`Canal: ${canal()}`}</p>
-        <p>{curso()}</p>
-      </header>
-      <section>
-        <img
-          src={LogoServ} // Dentro da pasta src
-          width={100}
-        />
-        <img
-          src='logo192.png' // Fora da pasta src (pasta public)
-        />
-      </section>
-    </section>
+    <>
+      <Header/> {/* Componente */}
+      <Corpo/>
+    </>
+    // <section>
+    //   <header>
+    //     <p>{`Canal: ${canal()}`}</p>
+    //     <p>{curso()}</p>
+    //   </header>
+    //   <section>
+    //     <img
+    //       src={LogoServ} // Dentro da pasta src
+    //       width={100}
+    //     />
+    //     <img
+    //       src='logo192.png' // Fora da pasta src (pasta public)
+    //     />
+    //   </section>
+    // </section>
   );
 };
